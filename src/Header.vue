5047af8f1663a8ea33a7cc00ad9415eb52601a46
uros <uros.isakovic98@gmail.com>
2020-05-03 20:10:03 +0200

    Added header and footer

--- src/Header.vue
+++ src/Header.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="app">
-    <h1>Header</h1>
+    <header class="site-header">
+			<nav class="navbar navbar-expand-md navbar-dark bg-steel fixed-top">
+				<div class="container">
+					<a class="navbar-brand mr-4" href="/">FUCA</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarToggle">
+						<div class="navbar-nav mr-auto">
+							<a class="nav-item nav-link" href="#"> Home</a>
+							<a class="nav-item nav-link" href="#"> Standings </a>
+							<a class="nav-item nav-link" href="#"> Results </a>
+							<a class="nav-item nav-link" href="#"> Schedule </a>
+							<a class="nav-item nav-link" href="#"> Teams </a>
+						</div>
+					<!-- Navbar Right Side -->
+					<div class="navbar-nav">
+						<a class="nav-item nav-link" href="#">Login</a>
+					</div>
+					</div>
+				</div>
+			</nav>
+    </header>
   </div>
 </template>
 

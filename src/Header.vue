c849622830d67316cba5fcf23fbf0ba39c47923f
uros <uros.isakovic98@gmail.com>
2020-05-03 20:00:43 +0200

    Removed starting components and added header and footer components

---
+++ src/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <h1>Header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+
+Vue.component('vheader', Header);
+Vue.component('vfooter', Footer);
+
 
 Vue.config.productionTip = false
 

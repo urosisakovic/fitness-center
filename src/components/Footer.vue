45f794c7920b77d1a3eb1208c730d6e3c601778f
uros <uros.isakovic98@gmail.com>
2020-05-04 17:17:14 +0200

    Updated header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,33 +3,30 @@
 		<div class="container-fluid text-center text-md-left">
 			<div class="row">
 				<div class="col-md-6 mt-md-0 mt-3">
-					<h5 class="text-uppercase">FUCA</h5>
-					<p>Recreational football league.</p>
+					<h5 class="text-uppercase">GYM</h5>
+					<p>The best gym out there.</p>
 				</div>
 				<hr class="clearfix w-100 d-md-none pb-3">
 				<div class="col-md-3 mb-md-0 mb-3">
-					<h5 class="text-uppercase">League</h5>
+					<h5 class="text-uppercase">Services</h5>
 					<ul class="list-unstyled">
-						<li><a href="#!">Home</a></li>
-						<li><a href="#!">Standings</a></li>
-						<li><a href="#!">Results</a></li>
-						<li><a href="#!">Schedule</a></li>
+						<li><router-link to="/services/training">Training</router-link></li>
+						<li><router-link to="/services/nutrtion">Nutrition</router-link></li>
+						<li><router-link to="/services/massage">Massage</router-link></li>
 					</ul>
 				</div>
 				<div class="col-md-3 mb-md-0 mb-3">
-					<h5 class="text-uppercase">Statistics</h5>
-
+					<h5 class="text-uppercase">Navigation</h5>
 					<ul class="list-unstyled">
-						<li><a href="#!">Stats</a></li>
-						<li><a href="#!">Best players</a></li>
-						<li><a href="#!">Top scorers</a></li>
-						<li><a href="#!">Teams</a></li>
+						<li><router-link to="/home">Home</router-link></li>
+						<li><router-link to="/about">About</router-link></li>
+						<li><router-link to="/account">Account</router-link></li>
 					</ul>
 				</div>  
 			</div>  
 		</div>
 
-		<div class="footer-copyright text-center py-3">© 2020 si3psi-LingLong:
+		<div class="footer-copyright text-center py-3">© 2020, Nikola Barjaktarevic, Uros Isakovic:
 			<a href="http://si3psi.etf.rs/"> http://si3psi.etf.rs/ </a>
 		</div>
 	</footer>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,22 +2,35 @@
     <header class="site-header">
 			<nav class="navbar navbar-expand-md navbar-dark bg-steel fixed-top">
 				<div class="container">
-					<a class="navbar-brand mr-4" href="/">FUCA</a>
+					<router-link class="navbar-brand mr-4" to="/home">GYM</router-link>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
 					<div class="collapse navbar-collapse" id="navbarToggle">
-						<div class="navbar-nav mr-auto">
+						<!-- Navbar Left Side -->
+						<div class="navbar-nav mr-auto"></div>
+						<!-- Navbar Right Side -->
+						<div class="navbar-nav">
 							<router-link class="nav-item nav-link" to="/home"> Home</router-link>
-							<a class="nav-item nav-link" href="#"> Standings </a>
-							<a class="nav-item nav-link" href="#"> Results </a>
-							<a class="nav-item nav-link" href="#"> Schedule </a>
-							<a class="nav-item nav-link" href="#"> Teams </a>
+							<div class="dropdown">
+								<a href="#" class="dropdown-toggle nav-item nav-link" data-toggle="dropdown">Services</a>
+								<div class="dropdown-menu">
+									<router-link to="/services/training" class="dropdown-item">Training</router-link>
+									<router-link to="/services/nutrition" class="dropdown-item">Nutrition</router-link>
+									<router-link to="/services/massage" class="dropdown-item">Massage</router-link>
+								</div>
+							</div>
+							<div class="dropdown">
+								<a href="#" class="dropdown-toggle nav-item nav-link" data-toggle="dropdown">Book</a>
+								<div class="dropdown-menu">
+									<router-link to="/book/training" class="dropdown-item">Training</router-link>
+									<router-link to="/book/nutrition" class="dropdown-item">Nutrition</router-link>
+									<router-link to="/book/massage" class="dropdown-item">Massage</router-link>
+								</div>
+							</div>
+							<router-link class="nav-item nav-link" to="/about"> About</router-link>
+							<router-link class="nav-item nav-link" to="/account"> Account</router-link>
 						</div>
-					<!-- Navbar Right Side -->
-					<div class="navbar-nav">
-						<a class="nav-item nav-link" href="#">Login</a>
-					</div>
 					</div>
 				</div>
 			</nav>

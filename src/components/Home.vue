8aaff4433d2bca9613e3af01bfbcc76735383953
uros <uros.isakovic98@gmail.com>
2020-05-11 16:59:32 +0200

    Having trouble loading images. Small changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="wrapper">
+    <h1>Text</h1>
     <p>
       {{text}}
     </p>
+    <h1>Best trainings</h1>
     <p v-for="training in best_trainings" :key="training.name">
       {{training.name}}
     </p>
+    <h1>Images</h1>
     <p v-for="image in images" :key="image.file">
-      {{image.file}}
+      <img :src="getImgUrl(image.file)" v-bind:alt="image.file">
     </p>
   </div>
 </template>
@@ -16,6 +19,11 @@
 export default {
   data() {
     return require('../assets/content/en/home.json');
+  },
+  methods: {
+    getImgUrl(file) {
+      return '../assets/images' + file;
+    }
   }
 }
 </script>

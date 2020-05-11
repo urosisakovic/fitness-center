7acda748f874e21a8bb4e19d3fef144ed98538a1
uros <uros.isakovic98@gmail.com>
2020-05-11 16:20:56 +0200

    Added content json files and loaded json content in home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="wrapper">
-    <h1>Home</h1>
+    <p>
+      {{text}}
+    </p>
+    <p v-for="training in best_trainings" :key="training.name">
+      {{training.name}}
+    </p>
+    <p v-for="image in images" :key="image.file">
+      {{image.file}}
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return require('../assets/content/en/home.json');
+  }
+}
+</script>

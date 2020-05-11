e1c458b70eb2c84a80be4936fd1a971c66e8b013
uros <uros.isakovic98@gmail.com>
2020-05-11 15:41:21 +0200

    Changes sidebar contents

--- src/components/parts/Sidebar.vue
+++ src/components/parts/Sidebar.vue
@@ -1,40 +1,49 @@
 <template>
     <nav id="sidebar">
         <div class="sidebar-header">
-            <h3>Bootstrap Slider</h3>
+            <h3>Trainings</h3>
         </div>
 
-        <ul class="lisst-unstyled components">
-            <p>The Providers</p>
-
+        <ul class="list-unstyled components">
             <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu">
-                    <li><a href="#">Home 1</a></li>
-                    <li><a href="#">Home 2</a></li>
+                <a href="#yoga-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Yoga</a>
+                <ul class="collapse list-unstyled" id="yoga-submenu">
+                    <li><a href="#">Yoga 1</a></li>
+                    <li><a href="#">Yoga 2</a></li>
+                    <li><a href="#">Yoga 3</a></li>
+                    <li><a href="#">Yoga 4</a></li>
                 </ul>
             </li>
 
             <li>
-                <a href="#">About</a>
-            </li>
-
-            <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                <ul class="collapse list-unstyled" id="pageSubmenu">
-                    <li><a href="#">Page 1</a></li>
-                    <li><a href="#">Page 2</a></li>
+                <a href="#pilates-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pilates</a>
+                <ul class="collapse list-unstyled" id="pilates-submenu">
+                    <li><a href="#">Pilates 1</a></li>
+                    <li><a href="#">Pilates 2</a></li>
+                    <li><a href="#">Pilates 3</a></li>
+                    <li><a href="#">Pilates 4</a></li>
                 </ul>
             </li>
 
             <li>
-                <a href="#">Policy</a>
+                <a href="#core-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Core</a>
+                <ul class="collapse list-unstyled" id="core-submenu">
+                    <li><a href="#">Core 1</a></li>
+                    <li><a href="#">Core 2</a></li>
+                    <li><a href="#">Core 3</a></li>
+                    <li><a href="#">Core 4</a></li>
+                </ul>
             </li>
 
             <li>
-                <a href="#">Contact Us</a>
+                <a href="#cardio-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Cardio</a>
+                <ul class="collapse list-unstyled" id="cardio-submenu">
+                    <li><a href="#">Cardio 1</a></li>
+                    <li><a href="#">Cardio 2</a></li>
+                    <li><a href="#">Cardio 3</a></li>
+                    <li><a href="#">Cardio 4</a></li>
+                </ul>
             </li>
-
         </ul>
     </nav>
 </template>

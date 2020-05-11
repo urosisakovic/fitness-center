09726f21c44cf386b824be66f97fc8ff95db9ae1
uros <uros.isakovic98@gmail.com>
2020-05-11 21:52:59 +0200

    Added cardio and core routes and data

--- src/routes.js
+++ src/routes.js
@@ -33,5 +33,17 @@ export default [
     {path: '/services/training/pilates/pilates-1', component: TrainingLvl2, props: {content: require("./assets/content/en/services/pilates/pilates-1.json")}},
     {path: '/services/training/pilates/pilates-2', component: TrainingLvl2, props: {content: require("./assets/content/en/services/pilates/pilates-2.json")}},
     {path: '/services/training/pilates/pilates-3', component: TrainingLvl2, props: {content: require("./assets/content/en/services/pilates/pilates-3.json")}},
-    {path: '/services/training/pilates/pilates-4', component: TrainingLvl2, props: {content: require("./assets/content/en/services/pilates/pilates-4.json")}}
+    {path: '/services/training/pilates/pilates-4', component: TrainingLvl2, props: {content: require("./assets/content/en/services/pilates/pilates-4.json")}},
+
+    {path: '/services/training/core', component: TrainingLvl1, props: {content: require("./assets/content/en/services/core/core.json")}},
+    {path: '/services/training/core/core-1', component: TrainingLvl2, props: {content: require("./assets/content/en/services/core/core-1.json")}},
+    {path: '/services/training/core/core-2', component: TrainingLvl2, props: {content: require("./assets/content/en/services/core/core-2.json")}},
+    {path: '/services/training/core/core-3', component: TrainingLvl2, props: {content: require("./assets/content/en/services/core/core-3.json")}},
+    {path: '/services/training/core/core-4', component: TrainingLvl2, props: {content: require("./assets/content/en/services/core/core-4.json")}},
+
+    {path: '/services/training/cardio', component: TrainingLvl1, props: {content: require("./assets/content/en/services/cardio/cardio.json")}},
+    {path: '/services/training/cardio/cardio-1', component: TrainingLvl2, props: {content: require("./assets/content/en/services/cardio/cardio-1.json")}},
+    {path: '/services/training/cardio/cardio-2', component: TrainingLvl2, props: {content: require("./assets/content/en/services/cardio/cardio-2.json")}},
+    {path: '/services/training/cardio/cardio-3', component: TrainingLvl2, props: {content: require("./assets/content/en/services/cardio/cardio-3.json")}},
+    {path: '/services/training/cardio/cardio-4', component: TrainingLvl2, props: {content: require("./assets/content/en/services/cardio/cardio-4.json")}}
 ]

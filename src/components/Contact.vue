b67621607f82d96c1a2a2dfbcb811057a4edb049
uros <uros.isakovic98@gmail.com>
2020-05-12 15:08:16 +0200

    Added contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
-    <h1>Map</h1>
-    <h1>Contact</h1>
-    <h2>Telephones</h2>
-    <h2>Emails</h2>
-    <h1>Address</h1>
+    <iframe id="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2831.107524818898!2d20.374148215312808!3d44.79899727909876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a6f0cf07bb83f%3A0xc6e9c7c92ccaeaeb!2sJurija%20Gagarina%20217%2C%20Beograd!5e0!3m2!1sen!2srs!4v1589287693967!5m2!1sen!2srs"
+    width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+    </iframe>
+
+    <div class="row">
+      <div class="col-md-4 text-center">
+        <h2>Address</h2>
+        <p>Jurija Gagarina 217</p>
+      </div>
+
+      <div class="col-md-4 text-center">
+        <h2>Email</h2>
+        <p>contact@bestgym.rs</p>
+      </div>
+
+      <div class="col-md-4 text-center">
+        <h2>Telephones</h2>
+        <p>
+          064/0555123<br>
+          064/0555123
+        </p>
+      </div>
+    </div>
+    
   </div>
 </template>
 

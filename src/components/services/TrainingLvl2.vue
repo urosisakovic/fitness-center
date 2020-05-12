bb4d362279a2b4a9978c38c5f59ec8bd77d58bd6
uros <uros.isakovic98@gmail.com>
2020-05-12 11:58:48 +0200

    Almost fixed sidebar

--- src/components/services/TrainingLvl2.vue
+++ src/components/services/TrainingLvl2.vue
@@ -1,7 +1,31 @@
 <template>
-    <div>
+    <div id="wrapper">
         <app-sidebar></app-sidebar>
-        <h1>{{content}}</h1>
+        
+        <div id="content">
+            <h1> {{content.name}} </h1>
+            <p> {{content.desc}} </p>
+            <p> {{content.duration}} </p>
+            <p> {{content.diff}} </p>
+            <p> {{content.avg_mark}} </p>
+
+            <h1>Images</h1>
+            <p v-for="image in content.images" :key="image.id">
+                {{image.file}}
+            </p>
+
+            <h1>Videos</h1>
+            <p v-for="video in content.videos" :key="video.id">
+                {{video.ogg}}
+            </p>
+
+            <h1>Comments</h1>
+            <p v-for="comment in content.comments" :key="comment.id">
+                {{comment.author}}<br />
+                {{comment.text}}<br />
+                {{comment.date}}
+            </p>    
+        </div>
     </div>
 </template>
 

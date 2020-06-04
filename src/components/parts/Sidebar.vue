ad75ac193836fb9bf9d22e50a61ec24d12915c03
Uros Isakovic <uros.isakovic98@gmail.com>
2020-06-04 11:30:16 +0200

    Added ugly html to training lvl1 pages

--- src/components/parts/Sidebar.vue
+++ src/components/parts/Sidebar.vue
@@ -8,6 +8,7 @@
             <li>
                 <a href="#yoga-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle-sb">Yoga</a>
                 <ul class="collapse list-unstyled" id="yoga-submenu">
+                    <li><router-link to="/services/training/yoga">Yoga Schedule</router-link></li>
                     <li><router-link to="/services/training/yoga/yoga-1">Yoga1</router-link></li>
                     <li><router-link to="/services/training/yoga/yoga-2">Yoga2</router-link></li>
                     <li><router-link to="/services/training/yoga/yoga-3">Yoga3</router-link></li>
@@ -17,6 +18,7 @@
             <li>
                 <a href="#pilates-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle-sb">Pilates</a>
                 <ul class="collapse list-unstyled" id="pilates-submenu">
+                    <li><router-link to="/services/training/pilates">Pilates Schedule</router-link></li>
                     <li><router-link to="/services/training/pilates/pilates-1">Pilates1</router-link></li>
                     <li><router-link to="/services/training/pilates/pilates-2">Pilates2</router-link></li>
                     <li><router-link to="/services/training/pilates/pilates-3">Pilates3</router-link></li>
@@ -26,6 +28,7 @@
             <li>
                 <a href="#core-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle-sb">Core</a>
                 <ul class="collapse list-unstyled" id="core-submenu">
+                    <li><router-link to="/services/training/core">Core Schedule</router-link></li>
                     <li><router-link to="/services/training/core/core-1">Core1</router-link></li>
                     <li><router-link to="/services/training/core/core-2">Core2</router-link></li>
                     <li><router-link to="/services/training/core/core-3">Core3</router-link></li>
@@ -35,6 +38,7 @@
             <li>
                 <a href="#cardio-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle-sb">Cardio</a>
                 <ul class="collapse list-unstyled" id="cardio-submenu">
+                    <li><router-link to="/services/training/cardio">Cardio Schedule</router-link></li>
                     <li><router-link to="/services/training/cardio/cardio-1">Cardio1</router-link></li>
                     <li><router-link to="/services/training/cardio/cardio-2">Cardio1</router-link></li>
                     <li><router-link to="/services/training/cardio/cardio-3">Cardio1</router-link></li>
--- src/components/services/TrainingLvl1.vue
+++ src/components/services/TrainingLvl1.vue
@@ -1,8 +1,62 @@
 <template>
-    <div>
+    <div id="wrapper">
         <app-sidebar></app-sidebar>
-        <div id="content">
-        <h1>{{content}}</h1>
+        
+        <div class="container" id="content">
+            <h1>{{content.name}} </h1>
+            <p> {{content.desc}} </p>
+
+
+            <h2> Choose your perfect training type! </h2>
+            <table>
+              <thead>
+                <tr>
+                  <td>Name</td>,
+                  <td>Duration</td>,
+                  <td>Difficulty</td>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr v-for="training in content.trainings" :key="training.id">
+                  <td>{{training.name}}</td>
+                  <td>{{training.duration}}</td>
+                  <td>{{training.diff}}</td>
+                </tr>
+              </tbody>
+
+            </table>
+
+            <!--Gallery-->
+            <div id="multi-item-example" class="carousel slide carousel-multi-item carousel-multi-item-2" data-ride="carousel">
+
+                <div class="controls-top">
+                    <a class="control-button" href="#multi-item-example" data-slide="prev"><i>&lt;</i></a>
+                    <a class="control-button" href="#multi-item-example" data-slide="next"><i>&gt;</i></a>
+                </div>
+
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <div class="col-md-3 mb-3" v-for="image in content.images" :key="image.id">
+                            <div class="card">
+                            <img class="img-fluid" :src="getImgUrl(image)">
+                                alt="Card image cap">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3 mb-3" v-for="image in content.images" :key="image.id">
+                            <div class="card">
+                            <img class="img-fluid" :src="getImgUrl(image)">
+                                alt="Card image cap">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!--/Gallery-->
+
         </div>
     </div>
 </template>
@@ -21,6 +75,11 @@ export default {
     props: {
         content: {
             required: true
+        }
+    },
+    methods: {
+        getImgUrl: function(pic) {
+            return require('../../assets/images/' + pic.file)
         }
     }
 }

15a4481e6d936ed7decda070e78a1b238602814a
barjak19a <nbarjakt@gmail.com>
2020-06-04 16:43:13 +0200

    UpdateSidebar and EditContent-Barjak

--- src/components/parts/Sidebar.vue
+++ src/components/parts/Sidebar.vue
@@ -9,9 +9,9 @@
                 <a href="#yoga-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle-sb">Yoga</a>
                 <ul class="collapse list-unstyled" id="yoga-submenu">
                     <li><router-link to="/services/training/yoga">Yoga Schedule</router-link></li>
-                    <li><router-link to="/services/training/yoga/yoga-1">Yoga1</router-link></li>
-                    <li><router-link to="/services/training/yoga/yoga-2">Yoga2</router-link></li>
-                    <li><router-link to="/services/training/yoga/yoga-3">Yoga3</router-link></li>
+                    <li><router-link to="/services/training/yoga/yoga-1">Classical Yoga</router-link></li>
+                    <li><router-link to="/services/training/yoga/yoga-2">Hatha Yoga</router-link></li>
+                    <li><router-link to="/services/training/yoga/yoga-3">Jain Yoga</router-link></li>
                 </ul>
             </li>
 
@@ -19,9 +19,9 @@
                 <a href="#pilates-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle-sb">Pilates</a>
                 <ul class="collapse list-unstyled" id="pilates-submenu">
                     <li><router-link to="/services/training/pilates">Pilates Schedule</router-link></li>
-                    <li><router-link to="/services/training/pilates/pilates-1">Pilates1</router-link></li>
-                    <li><router-link to="/services/training/pilates/pilates-2">Pilates2</router-link></li>
-                    <li><router-link to="/services/training/pilates/pilates-3">Pilates3</router-link></li>
+                    <li><router-link to="/services/training/pilates/pilates-1">Classical Pilates</router-link></li>
+                    <li><router-link to="/services/training/pilates/pilates-2">Mat Pilates</router-link></li>
+                    <li><router-link to="/services/training/pilates/pilates-3">Reformer Pilates</router-link></li>
                 </ul>
             </li>
 
@@ -29,9 +29,9 @@
                 <a href="#core-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle-sb">Core</a>
                 <ul class="collapse list-unstyled" id="core-submenu">
                     <li><router-link to="/services/training/core">Core Schedule</router-link></li>
-                    <li><router-link to="/services/training/core/core-1">Core1</router-link></li>
-                    <li><router-link to="/services/training/core/core-2">Core2</router-link></li>
-                    <li><router-link to="/services/training/core/core-3">Core3</router-link></li>
+                    <li><router-link to="/services/training/core/core-1">Strengthening back musculature</router-link></li>
+                    <li><router-link to="/services/training/core/core-2">Intra-abdominal pressure</router-link></li>
+                    <li><router-link to="/services/training/core/core-3">Martial Arts Core Excercises</router-link></li>
                 </ul>
             </li>
 
@@ -39,9 +39,9 @@
                 <a href="#cardio-submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle-sb">Cardio</a>
                 <ul class="collapse list-unstyled" id="cardio-submenu">
                     <li><router-link to="/services/training/cardio">Cardio Schedule</router-link></li>
-                    <li><router-link to="/services/training/cardio/cardio-1">Cardio1</router-link></li>
-                    <li><router-link to="/services/training/cardio/cardio-2">Cardio1</router-link></li>
-                    <li><router-link to="/services/training/cardio/cardio-3">Cardio1</router-link></li>
+                    <li><router-link to="/services/training/cardio/cardio-1">Sprinting</router-link></li>
+                    <li><router-link to="/services/training/cardio/cardio-2">High-intensity interval training</router-link></li>
+                    <li><router-link to="/services/training/cardio/cardio-3">Cycling</router-link></li>
                 </ul>
             </li>
         </ul>

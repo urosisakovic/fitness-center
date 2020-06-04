ee440fb525923b03d056a2c544ddbdf764af7dc5
Uros Isakovic <uros.isakovic98@gmail.com>
2020-06-04 12:08:31 +0200

    Started writing training booking page

--- src/components/book/Training.vue
+++ src/components/book/Training.vue
@@ -1,20 +1,76 @@
 <template>
-  <div id="wrapper">
-    <app-filterbar></app-filterbar>
+  <div class="container">
+    <h2> Choose your training! </h2>
 
-    <div id="content">
-    </div>
+    <select class="form-control">
+      <option>Yoga</option>
+    </select>
+
+    <select class="form-control">
+      <option>Yoga-1</option>
+    </select>
+
+    <h2> Available sessions: </h2>
+    <table>
+      <thead>
+        <tr>
+          <td>Type</td>
+          <td>Name</td>
+          <td>Date</td>
+          <td>Time</td>
+          <td>Places left</td>
+          <td>Book</td>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="training in trainings" :key="training.id">
+          <td> {{ training.type }} </td>
+          <td> {{ training.subtype }}</td>
+          <td> {{ training.date }} </td>
+          <td> {{ training.time }}</td>
+          <td> {{ training.placesLeft }}</td>
+        </tr>
+      </tbody>
+
+    </table>
+
   </div>
 </template>
 
 <script>
-import Filterbar from '../parts/Filterbar.vue'
-
 export default {
-  components: {
-    'app-filterbar': Filterbar
-  },
   data() {
+    return {
+      "trainings": [
+        {
+          "id": "0",
+          "type": "Yoga",
+          "subtype": "Yoga-1",
+          "date": "20-06-2020",
+          "time": "20:00",
+          "placesLeft": 5
+        },
+        {
+          "id": "1",
+          "type": "Yoga",
+          "subtype": "Yoga-2",
+          "date": "22-06-2020",
+          "time": "21:30",
+          "placesLeft": 12
+        }
+      ]
+    }
+  },
+  props: {
+    content: {
+      required: true
+    }
+  },
+  methods: {
+    refreshTable: function() {
+        return "Yoga"
+    }
   }
 }
 </script>

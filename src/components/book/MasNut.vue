033767711cd62cd14d192c79de6667025795c8da
Uros Isakovic <uros.isakovic98@gmail.com>
2020-06-05 18:18:25 +0200

    Added basic pdf creation and download

--- src/components/book/MasNut.vue
+++ src/components/book/MasNut.vue
@@ -27,15 +27,16 @@
           <textarea class="form-control" id="problem-desc" rows="3" :value="words.enterProblemDesc"></textarea>
         </div>
         <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">{{ words.downloadPdf }}</label>
+          <input type="checkbox" class="form-check-input" id="download-pdf">
+          <label class="form-check-label" for="download-pdf">{{ words.downloadPdf }}</label>
         </div>
-        <button id="masnut-submit-button" type="submit" class="btn btn-primary">{{ words.submit }}</button>
+        <button type="button" id="masnut-submit-button" class="btn btn-primary" v-on:click="submitButton()">{{ words.submit }}</button>
     </form>
   </div>
 </template>
 
 <script>
+import jsPDF from 'jspdf'
 export default {
   data() {
       return {
@@ -57,17 +58,41 @@ export default {
 				return false;
 			else
 				return true;
+    },
+
+    submitButton: function() {
+      console.log("Submit Button Clicked");
+
+      var downloadPDF = document.getElementById("download-pdf").checked;
+      if (downloadPDF)
+        this.savePDF();
+    },
+
+    savePDF: function() {
+      var doc = new jsPDF()
+ 
+      var firstName = document.getElementById("input-fn").value;
+      var lastName = document.getElementById("input-sn").value;
+      var telephone = document.getElementById("input-tel").value;
+      var email = document.getElementById("input-email").value;
+      var problemDesc = document.getElementById("problem-desc").value;
+
+      doc.text("GYM", 50, 10);
+      doc.text("You have successfully booked our services!", 10, 20)
+
+      doc.text("First Name: " + firstName,            10, 30);
+      doc.text("Last Name: " + lastName,              10, 40);
+      doc.text("Telephone: " + telephone,             10, 50);
+      doc.text("Email: " + email,                     10, 60);
+      doc.text("Problem Description: " + problemDesc, 10, 70);
+      doc.save('GYM.pdf')
     }
   },
-	mounted() {
-		if (this.englishLanguage()) {
-			console.log("English");
+  mounted() {
+		if (this.englishLanguage()) 
 			this.words = require("../../assets/content/en/dictionary.json").book
-		}
-		else {
-			console.log("Serbian");
+		else 
 			this.words = require("../../assets/content/sr/dictionary.json").book
-		}
 	}
 }
 </script>

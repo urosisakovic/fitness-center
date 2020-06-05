ff27f90cbd4e58260c87d5a8a9d1693673ff32e8
Uros Isakovic <uros.isakovic98@gmail.com>
2020-06-05 11:47:04 +0200

    Translated booking massage and nutritionist pages

--- src/components/book/MasNut.vue
+++ src/components/book/MasNut.vue
@@ -6,45 +6,68 @@
       </p>
       <form class="container">
         <div class="form-group">
-          <label for="input-fn">First Name</label>
-          <input type="text" class="form-control" id="input-fn" placeholder="Enter First Name">
+          <label for="input-fn">{{ words.fn }}</label>
+          <input type="text" class="form-control" id="input-fn" :placeholder="words.enterFn">
         </div>
         <div class="form-group">
-          <label for="input-sn">Second Name</label>
-          <input type="text" class="form-control" id="input-sn" placeholder="Enter Second Name">
+          <label for="input-sn">{{ words.sn }}</label>
+          <input type="text" class="form-control" id="input-sn" :placeholder="words.enterSn">
         </div>
         <div class="form-group">
-          <label for="input-tel">Telephone Number</label>
-          <input type="text" class="form-control" id="input-tel" placeholder="Enter Telephone Number">
+          <label for="input-tel">{{ words.tn }}</label>
+          <input type="text" class="form-control" id="input-tel" :placeholder="words.enterTn">
         </div>
         <div class="form-group">
-          <label for="input-email">Email address</label>
-          <input type="email" class="form-control" id="input-email" aria-describedby="email-help" placeholder="Enter Email">
-          <small id="email-help" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          <label for="input-email">{{ words.email }}</label>
+          <input type="email" class="form-control" id="input-email" aria-describedby="email-help" :placeholder="words.enterEmail">
+          <small id="email-help" class="form-text text-muted">{{ words.emailPrivacy }}</small>
         </div>
         <div class="form-group">
-          <label for="problem-desc">Short problem description</label>
-          <textarea class="form-control" id="problem-desc" rows="3">Enter Short Problem Description</textarea>
+          <label for="problem-desc">{{ words.problemDesc }}</label>
+          <textarea class="form-control" id="problem-desc" rows="3" :value="words.enterProblemDesc"></textarea>
         </div>
         <div class="form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Download PDF report</label>
+          <label class="form-check-label" for="exampleCheck1">{{ words.downloadPdf }}</label>
         </div>
-        <button id="masnut-submit-button" type="submit" class="btn btn-primary">Submit</button>
+        <button id="masnut-submit-button" type="submit" class="btn btn-primary">{{ words.submit }}</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-        }
-    },
-    props: {
-        content: {
-            required: true
-        }
+  data() {
+      return {
+        words: {}
+      }
+  },
+  props: {
+      content: {
+          required: true
+      }
+  },
+  methods: {
+    englishLanguage: function() {
+			var currUrl = this.$route.path;
+
+			if (currUrl.startsWith("/en"))
+				return true;
+			else if (currUrl.startsWith("/sr"))
+				return false;
+			else
+				return true;
     }
+  },
+	mounted() {
+		if (this.englishLanguage()) {
+			console.log("English");
+			this.words = require("../../assets/content/en/dictionary.json").book
+		}
+		else {
+			console.log("Serbian");
+			this.words = require("../../assets/content/sr/dictionary.json").book
+		}
+	}
 }
 </script>

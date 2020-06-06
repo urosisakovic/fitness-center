03a27426319e67094dc106211aae8f47d0b82020
Uros Isakovic <uros.isakovic98@gmail.com>
2020-06-06 17:50:22 +0200

    Added better form check in booking pages

--- src/components/book/MasNut.vue
+++ src/components/book/MasNut.vue
@@ -8,19 +8,22 @@
         <div class="form-group">
           <label for="input-fn">{{ words.fn }}</label>
           <input type="text" class="form-control" id="input-fn" :placeholder="words.enterFn">
+          <small id="input-fn-invalid" class="form-text text-muted" style="color: red !important;"></small>
         </div>
         <div class="form-group">
           <label for="input-sn">{{ words.sn }}</label>
           <input type="text" class="form-control" id="input-sn" :placeholder="words.enterSn">
+          <small id="input-sn-invalid" class="form-text text-muted" style="color: red !important;"></small>
         </div>
         <div class="form-group">
           <label for="input-tel">{{ words.tn }}</label>
           <input type="text" class="form-control" id="input-tel" :placeholder="words.enterTn">
+          <small id="input-tel-invalid" class="form-text text-muted" style="color: red !important;"></small>
         </div>
         <div class="form-group">
           <label for="input-email">{{ words.email }}</label>
-          <input type="email" class="form-control" id="input-email" aria-describedby="email-help" :placeholder="words.enterEmail">
-          <small id="email-help" class="form-text text-muted">{{ words.emailPrivacy }}</small>
+          <input type="text" class="form-control" id="input-email" aria-describedby="email-invalid" :placeholder="words.enterEmail">
+          <small id="email-invalid" class="form-text text-muted">{{ words.emailPrivacy }}</small>
         </div>
         <div class="form-group">
           <label for="problem-desc">{{ words.problemDesc }}</label>
@@ -60,8 +63,78 @@ export default {
 				return true;
     },
 
+    validEmail: function(emailToTest) {
+      // check for @
+      var atSymbol = emailToTest.indexOf("@");
+      if(atSymbol < 0) return false;
+
+      var dot = emailToTest.lastIndexOf(".");
+      if(dot <= atSymbol + 2) return false;
+
+      // check that the dot is not at the end
+      if (dot === emailToTest.length - 1) return false;
+
+      return true;
+    },
+
+    validTelephone: function(telephone) {
+      if (telephone == "")
+        return false;
+
+      if (telephone[0] != '+' && !(telephone[0] >= '0' && telephone[0] <= '9'))
+        return false;
+
+      for (var i = 1; i < telephone.length; i++)
+        if (!(telephone[i] >= '0' && telephone[i] <= '9'))
+          return false;
+
+      return true;
+    },
+
     submitButton: function() {
-      console.log("Submit Button Clicked");
+      var firstName = document.getElementById("input-fn").value;
+      var lastName = document.getElementById("input-sn").value;
+      var telephone = document.getElementById("input-tel").value;
+      var email = document.getElementById("input-email").value;
+
+      var valid = true;
+
+      if (firstName == "") {
+        document.getElementById("input-fn-invalid").innerHTML = "Invalid first name.";
+        valid = false;
+      }
+      else {
+        document.getElementById("input-fn-invalid").innerHTML = "";
+      }
+
+      if (lastName == "") {
+        document.getElementById("input-sn-invalid").innerHTML = "Invalid second name.";
+        valid = false;
+      }
+      else {
+        document.getElementById("input-sn-invalid").innerHTML = "";
+      }
+
+      if (!this.validTelephone(telephone)) {
+        document.getElementById("input-tel-invalid").innerHTML = "Invalid telephone number.";
+        valid = false;
+      }
+      else {
+        document.getElementById("input-tel-invalid").innerHTML = "";
+      }
+
+       if (!this.validEmail(email)) {
+        document.getElementById("email-invalid").innerHTML = "Invalid email.";
+        document.getElementById("email-invalid").style.setProperty("color", "red", "important");
+        valid = false;
+      }
+      else {
+        document.getElementById("email-invalid").innerHTML = this.words.emailPrivacy;
+        document.getElementById("email-invalid").style.setProperty("color", "#333333");
+      }
+
+      if (!valid)
+        return;
 
       var downloadPDF = document.getElementById("download-pdf").checked;
       if (downloadPDF)

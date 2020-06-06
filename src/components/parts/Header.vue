2b6e6a19e56beeb9f50080bd5e708af4ad85a51b
Uros Isakovic <uros.isakovic98@gmail.com>
2020-06-06 21:05:09 +0200

    Added banner

--- src/components/parts/Header.vue
+++ src/components/parts/Header.vue
@@ -1,41 +1,44 @@
 <template>
-    <header class="site-header" :key="language">
-			<nav class="navbar navbar-expand-md navbar-dark bg-steel fixed-top">
-				<div class="container-fluid">
-					<router-link class="navbar-brand mr-4" :to="makeLanguageRoute(homeLink)">GYM</router-link>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse navbar-collapse" id="navbarToggle">
-						<!-- Navbar Left Side -->
-						<div class="navbar-nav mr-auto"></div>
-						<!-- Navbar Right Side -->
-						<div class="navbar-nav">
-							<router-link class="nav-item nav-link" :to="makeLanguageRoute(homeLink)"> {{ words.home }} </router-link>
-							<div class="dropdown">
-								<a href="#" class="dropdown-toggle nav-item nav-link" data-toggle="dropdown">{{ words.services }}</a>
-								<div class="dropdown-menu">
-									<router-link :to="makeLanguageRoute(serviceTrainingLink)" class="dropdown-item">{{ words.training }}</router-link>
-									<router-link :to="makeLanguageRoute(serviceNutritionLink)" class="dropdown-item">{{ words.nutrition }}</router-link>
-									<router-link :to="makeLanguageRoute(serviceMassageLink)" class="dropdown-item">{{ words.massage }}</router-link>
-								</div>
+    <header class="site-header fixed-top" :key="language">
+
+		<div class="jumbotron" id="banner" :style="{ backgroundImage: `url( ${image} )` }"></div>
+
+		<nav class="navbar navbar-expand-md navbar-dark bg-steel">
+			<div class="container-fluid">
+				<router-link class="navbar-brand mr-4" :to="makeLanguageRoute(homeLink)">GYM</router-link>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarToggle">
+					<!-- Navbar Left Side -->
+					<div class="navbar-nav mr-auto"></div>
+					<!-- Navbar Right Side -->
+					<div class="navbar-nav">
+						<router-link class="nav-item nav-link" :to="makeLanguageRoute(homeLink)"> {{ words.home }} </router-link>
+						<div class="dropdown">
+							<a href="#" class="dropdown-toggle nav-item nav-link" data-toggle="dropdown">{{ words.services }}</a>
+							<div class="dropdown-menu">
+								<router-link :to="makeLanguageRoute(serviceTrainingLink)" class="dropdown-item">{{ words.training }}</router-link>
+								<router-link :to="makeLanguageRoute(serviceNutritionLink)" class="dropdown-item">{{ words.nutrition }}</router-link>
+								<router-link :to="makeLanguageRoute(serviceMassageLink)" class="dropdown-item">{{ words.massage }}</router-link>
 							</div>
-							<div class="dropdown">
-								<a href="#" class="dropdown-toggle nav-item nav-link" data-toggle="dropdown">{{ words.book }}</a>
-								<div class="dropdown-menu">
-									<router-link :to="makeLanguageRoute(bookTrainingLink)" class="dropdown-item">{{ words.training }}</router-link>
-									<router-link :to="makeLanguageRoute(bookNutritionLink)" class="dropdown-item">{{ words.nutrition }}</router-link>
-									<router-link :to="makeLanguageRoute(bookMassageLink)" class="dropdown-item">{{ words.massage }}</router-link>
-								</div>
-							</div>
-							<router-link class="nav-item nav-link" :to="makeLanguageRoute(aboutLink)"> {{ words.aboutus }} </router-link>
-							<router-link class="nav-item nav-link" :to="makeLanguageRoute(contactLink)"> {{ words.contact }} </router-link>
-							<router-link class="nav-item nav-link" :to="makeLanguageRoute(accoutLink)"> {{ words.myaccount }} </router-link>
-							<router-link class="nav-item nav-link" :to="swapLanguageURL().url"> SR|EN </router-link>
 						</div>
+						<div class="dropdown">
+							<a href="#" class="dropdown-toggle nav-item nav-link" data-toggle="dropdown">{{ words.book }}</a>
+							<div class="dropdown-menu">
+								<router-link :to="makeLanguageRoute(bookTrainingLink)" class="dropdown-item">{{ words.training }}</router-link>
+								<router-link :to="makeLanguageRoute(bookNutritionLink)" class="dropdown-item">{{ words.nutrition }}</router-link>
+								<router-link :to="makeLanguageRoute(bookMassageLink)" class="dropdown-item">{{ words.massage }}</router-link>
+							</div>
+						</div>
+						<router-link class="nav-item nav-link" :to="makeLanguageRoute(aboutLink)"> {{ words.aboutus }} </router-link>
+						<router-link class="nav-item nav-link" :to="makeLanguageRoute(contactLink)"> {{ words.contact }} </router-link>
+						<router-link class="nav-item nav-link" :to="makeLanguageRoute(accoutLink)"> {{ words.myaccount }} </router-link>
+						<router-link class="nav-item nav-link" :to="swapLanguageURL().url"> SR|EN </router-link>
 					</div>
 				</div>
-			</nav>
+			</div>
+		</nav>
     </header>
 </template>
 
@@ -53,6 +56,7 @@ export default {
 			aboutLink: "/about",
 			contactLink: "/contact",
 			accoutLink: "/account",
+			image: require("../../assets/images/banner.png"),
 			words: {}
         }
     },
@@ -95,14 +99,10 @@ export default {
 		}
 	},
 	mounted() {
-		if (this.englishLanguage()) {
-			console.log("English");
+		if (this.englishLanguage())
 			this.words = require("../../assets/content/en/dictionary.json").header_footer
-		}
-		else {
-			console.log("Serbian");
+		else
 			this.words = require("../../assets/content/sr/dictionary.json").header_footer
-		}
 	}
 }
 </script>
